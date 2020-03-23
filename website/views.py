5c8de09933a97c93e7ddf343949cbbec04aa8d88
Josh Owens <jowens@dealerinspire.com>
2020-03-23 14:36:43 -0500

    Adding about section

--- website/views.py
+++ website/views.py
@@ -7,5 +7,10 @@ from django.shortcuts import render
 def welcome(request):
     return HttpResponse("Welcome to the Meeting Planner!")
 
+
 def date(request):
     return HttpResponse("This page was served at: " + str(datetime.now()))
+
+
+def about(request):
+    return HttpResponse("I'm Josh and I'm learning django")
