47f1640cee766eb2f3486e03757c91beeaaaf92f
Josh Owens <jowens@dealerinspire.com>
2020-03-24 15:33:09 -0500

    removing comments

--- meetings/views.py
+++ meetings/views.py
@@ -4,7 +4,6 @@ from meetings.models import Meeting
 
 
 def detail(request, id):
-    # meeting = Meeting.objects.get(pk=id)
     meeting = get_object_or_404(Meeting, pk=id)
     return render(request, "meetings/detail.html", {"meeting": meeting})
 
